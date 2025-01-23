20aecabadaaa5a210b71a2503885f6b1cf4d0a46
Mike O'Leary <michael.oleary@windowslive.com>
2025-01-23 18:31:36 +1300

    Add example

---
+++ ILRepackHotReload/Program.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.Extensions.Logging;
+
+var logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger<Program>();
+logger.LogInformation("Hello, World!");
+//logger.LogInformation("Attempting hot reload...");
